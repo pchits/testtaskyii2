e15c256d61c92c2e7e7ed86951bee9eb7744649a
Fedor Tokarev <>
2019-11-09 17:16:55 +0300

    Console comand to send reward

--- basic/commands/HelloController.php
+++ basic/commands/HelloController.php
@@ -9,6 +9,7 @@ namespace app\commands;
 
 use yii\console\Controller;
 use yii\console\ExitCode;
+use app\models\Game;
 
 /**
  * This command echoes the first argument that you have entered.
@@ -28,6 +29,39 @@ class HelloController extends Controller
     public function actionIndex($message = 'hello world')
     {
         echo $message . "\n";
+
+        return ExitCode::OK;
+    }
+
+    /**
+     * Send money to user accounts
+     * @return int Exit code
+     */
+    public function actionReward($N = 10)
+    {
+
+        $query = Game::find()
+            ->where(['status' => 'NEW'])
+            ->andWhere(['prise_type' => 'money'])
+            ->limit($N)->all();
+
+        echo "Games to reward: " . sizeof($query) . "\n";
+
+        $ids = [];
+
+        foreach ($query as $rows) {
+            $ids[] = $rows['id'];
+
+            //SEND REQUEST RO BANK
+            // ->
+            //HERE
+
+            $game = Game::find()
+                ->where(['id' => $rows['id']])
+                ->one();
+            $game->status = 'DONE';
+            $game->save();
+        }
 
         return ExitCode::OK;
     }
